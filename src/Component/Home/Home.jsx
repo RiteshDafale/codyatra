c1d758b7e6079a3d74ff57a965aea292746cb47a
Ritesh Dafale <ritesh.dafale13@gmail.com>
2025-08-26 09:23:07 +0530

    ..

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import Navigation from '../Header/Navigation'
+import React, { useEffect } from 'react'
 import Banner from '../Banner/Banner'
 import Services from '../Services/Services'
 import Why from '../Why/Why'
-import Footer from '../Footer/Footer'
 import NewService from '../Services/NewServices1'
 import OwnerDetails from '../Owner/OwnerDetails'
 
 function Home() {
+
+  useEffect(() => {
+    document.title = "Codyatra - Digital Marketting"
+  }, [])
+
+
   return (
-    <div>    
+    <div>
       <Banner />
       {/* <Services /> */}
-      <NewService/>
-      <OwnerDetails/>
+      <NewService />
+      <OwnerDetails />
       <Why />
-      
+
     </div>
   )
 }
--- src/Component/Services/NewServices1.jsx
+++ src/Component/Services/NewServices1.jsx
@@ -14,43 +14,59 @@ function NewServices1() {
       <div className='mainHeading'>
         <h1 className="text-center ">Our Services</h1>
       </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       {/* First Row */}
-      <div className="row firstrow bg-primary ">
+      <div className="row firstrow ">
         {/* First Column */}
-        <div className="col-lg-4  col-sm-12 first column  d-flex justify-content-center align-items-center ">
-          <div className="firstcontent  ">
+        <div className="col-lg-4  col-sm-12 first servicecolumn ">
+          <div className="firstcontent d-flex flex-column justify-content-center align-items-center  ">
             <div className="img">
               <img src={web} alt="Website Development" className="img-fluid" />
             </div>
-            <div className="heading">
-              <h6 className="text-center ">Website &amp; App Development</h6>
+            <div className="heading mt-2">
+              <h5 className="text-center ">Website &amp; App Development</h5>
             </div>
           </div>
         </div>
 
         {/* Second Column */}
-        <div className="col-lg-4  col-sm-12 column digital">
-          <div className="SecondMiDiv ">
-            <div className="second ">
-              <div className="img">
-                <img
-                  src={DigitalMarketting}
-                  alt="Digital Marketing"
-                  className="img-fluid"
-                />
-              </div>
-              <div className="heading border w-100 text-center">
-                <h6 className="">Digital Marketing &amp; Advertising</h6>
-              </div>
+        <div className="col-lg-4  col-sm-12 servicecolumn ">
+          <div className="SecondMiDiv  d-flex flex-column justify-content-center align-items-center  ">
+            <div className="img">
+              <img
+                src={DigitalMarketting}
+                alt="Digital Marketing"
+                className="img-fluid"
+              />
+            </div>
+            <div className="heading border w-100 text-center mt-2">
+              <h5 className="">Digital Marketing &amp; Advertising</h5>
             </div>
           </div>
         </div>
 
         {/* Third Column */}
-        <div className="col-lg-4  col-sm-12 column ">
-          <div className="">
-            <div className="third ">
-              <div className="img d-flex " >
+        <div className="col-lg-4  col-sm-12 servicecolumn  d-flex flex-column justify-content-center align-items-center   ">
+          <div>
+            <div className="third d-flex flex-column justify-content-center align-items-center   ">
+              <div className="img  " >
                 <img
                   src={socialMedia}
                   alt="Social Media Management"
@@ -58,30 +74,30 @@ function NewServices1() {
                 />
               </div>
               <div className="heading_social border">
-                <h6 className="text-center">Social xMedia Management &amp; Content Creation</h6>
+                <h5 className="text-center">Social xMedia Management &amp; Content Creation</h5>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       {/* second row */}
       <div className="row secondrow mt-3">
         {/* First Column */}
-        <div className="col-lg-4  col-sm-12 first column">
-          <div className="photoService ">
+        <div className="col-lg-4  col-sm-12 first servicecolumn">
+          <div className="photoService d-flex flex-column justify-content-center align-items-center  ">
             <div className="img">
               <img src={photography} alt="Website Development" className="img-fluid" />
             </div>
             <div className="heading w-100 p-2">
-              <h6 className="text-center">Video &amp; Photography (On Request / City-Specific)</h6>
+              <h5 className="text-center">Video &amp; Photography (On Request / City-Specific)</h5>
             </div>
           </div>
         </div>
 
         {/* Second Column */}
-        <div className="col-lg-4  col-sm-12 column digital">
-          <div className="SecondMiDiv">
-            <div className="second">
+        <div className="col-lg-4  col-sm-12 servicecolumn digital">
+          <div className="SecondMiDiv d-flex flex-column justify-content-center align-items-center ">
               <div className="img">
                 <img
                   src={strategy}
@@ -90,17 +106,15 @@ function NewServices1() {
                 />
               </div>
               <div className="heading border w-100 text-center p-2">
-                <h6 className="">Strategy &amp; Consulting</h6>
+                <h5 className="">Strategy &amp; Consulting</h5>
               </div>
-            </div>
           </div>
         </div>
 
         {/* Third Column */}
-        <div className="col-lg-4  col-sm-12 column ">
-          <div className="">
-            <div className="third ">
-              <div className="img d-flex justicy-content-center align-items-center w-100" >
+        <div className="col-lg-4  col-sm-12 servicecolumn  ">
+            <div className="third d-flex flex-column justify-content-center align-items-center  ">
+              <div className="img " >
                 <img
                   src={free}
                   alt="Social Media Management"
@@ -108,9 +122,8 @@ function NewServices1() {
                 />
               </div>
               <div className="heading_social border">
-                <h6 className="text-center">Freebies &amp; Entry Services (Great for First-Time Clients)</h6>
+                <h5 className="text-center">Freebies &amp; Entry Services (Great for First-Time Clients)</h5>
               </div>
-            </div>
           </div>
         </div>
       </div>
