a616f6cc885de9965c2230fc4eea3c9f7d7fffd1
Ritesh Dafale <ritesh.dafale13@gmail.com>
2025-09-05 09:34:12 +0530

    lazy load on service Page

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,Suspense  } from 'react'
 import Banner from '../Banner/Banner'
 import Services from '../Services/Services'
 import Why from '../Why/Why'
-import NewService from '../Services/NewServices1'
+// import NewService from '../Services/NewServices1'    // lazy load 
+const NewService = React.lazy(()=>import('../Services/NewServices1'));
 import OwnerDetails from '../Owner/OwnerDetails'
 
 function Home() {
@@ -17,7 +18,10 @@ function Home() {
       {/* <Banner /> */}
       {/* <Services /> */}
       <Why />
+      <Suspense  fallback={<div>Loading Please wait ...</div>}>
       <NewService />
+      
+      </Suspense>  
       <OwnerDetails />
     </div>
   )
