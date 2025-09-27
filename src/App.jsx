21865d941bb0bf690c5bcda1aeede691ea61f4f8
Ritesh Dafale <ritesh.dafale13@gmail.com>
2025-09-27 12:26:13 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Home from './Component/Home/Home'
 import Navigation from './Component/Header/Navigation'
 import './App.css'
 import Footer from './Component/Footer/Footer'
-const About = React.lazy(()=> import ('./Component/About/About'));
-const NewWhy = React.lazy(()=> import( './Component/Why/NewWhy'));
-const OurServices = React.lazy(()=> import('./Component/RedirectPages/OurServices/OurServices'));
+const About = React.lazy(() => import('./Component/About/About'));
+const NewWhy = React.lazy(() => import('./Component/Why/NewWhy'));
+const OurServices = React.lazy(() => import('./Component/RedirectPages/OurServices/OurServices'));
 const Testimonial = React.lazy(() => import('./Component/Testimonial/Testimonial'));
-const Pricing = React.lazy(()=>import('./Component/PricingPlan/Pricing'));
-const Dashboard = React.lazy(()=> import('./Component/Dashboard/Dashboard')); 
-const FreeTools = React.lazy(()=> import ('./Component/FreeTools/FreeTools')) ;
-const Contact = React.lazy(()=> import('./Component/Contact/Contact.JSX'));
+const Pricing = React.lazy(() => import('./Component/PricingPlan/Pricing'));
+const Dashboard = React.lazy(() => import('./Component/Dashboard/Dashboard'));
+const FreeTools = React.lazy(() => import('./Component/FreeTools/FreeTools'));
+const Contact = React.lazy(() => import('./Component/Contact/Contact.JSX'));
 import { ToastContainer } from 'react-toastify';
 
 
@@ -23,7 +23,7 @@ function App() {
   // background-color: rgb(25 48 96);
   return (
     <div className='mainBackgroundColor' style={{ color: "white" }}>
-    
+
 
       <ToastContainer
         position="top-right"
@@ -43,6 +43,7 @@ function App() {
       <Suspense fallback={<div>Loading ...</div>}>
         {/* <DarkVeil /> */}
         <Routes>
+          <Route path='/' element={<home />} />
           <Route path="/codyatra" element={<Home />}></Route>
           <Route path="/codyatra/about" element={<About />} />
           <Route path='/codyatra/Why' element={<NewWhy />} />
