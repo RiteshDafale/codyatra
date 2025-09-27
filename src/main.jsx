90efe646946355d795bd21898fa5b1dc2d69acff
Ritesh Dafale <ritesh.dafale13@gmail.com>
2025-09-27 12:16:59 +0530

    testing reload 404 error

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import {BrowserRouter, HashRouter} from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>
+  </StrictMode>,
 )
