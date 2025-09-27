40b4b536a19125dc043adc1ca30d089f1555259c
Ritesh Dafale <ritesh.dafale13@gmail.com>
2025-09-27 12:04:53 +0530

    Updates

---
+++ assets/Dashboard-DSSOzfCB.js
@@ -0,0 +1,1 @@
+import{r as e,j as t}from"./index-CFElSbsW.js";function r(){return e.useState(()=>{document.title="Dashboard - Codyatra"}),t.jsx("div",{children:t.jsx("h3",{className:"text-danger text-center my-4",style:{height:"100vh"},children:"Comming soon --- under Maintainance"})})}export{r as default};
